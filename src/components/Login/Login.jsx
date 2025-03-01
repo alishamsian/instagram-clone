6c8972fdf1e6e88abae3dc108d669d81147f99a8
mahdi <mahdi.amini8894@gmail.com>
2025-03-01 14:02:39 +0330

    end login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import Logo from "../../assets/logo.png";
 import LoginImg from "../../assets/login-img.png";
+import { toast } from "react-toastify";
+import { client } from "../../lib/axios";
+import * as yup from "yup"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "react-router";
+
 
 export default function Login() {
+  const schema=yup.object({
+    username:yup.string().required(),
+    password:yup.string().required().min(8).max(16)
+})
+const{register,handleSubmit,formState:{errors}}=useForm({//هوک برای مقداردهی و مدیریت ورودی های فرم استفاده شده استuseForm 
+  resolver:yupResolver(schema),
+  mode:"onBlur"
+})
+async function submitForm(user){//خروجیش object
+  try {
+      
+      console.log(user);
+     const response=await client.post("/user/login", user)
+     localStorage.setItem('token',response.data.jwt)
+
+     toast.success("user added successfull",{
+      type:"success",
+     })
+  } catch (error) {
+      toast.error("error",{
+          type:"error"
+      });
+  }
+}
+
   return (
     <div className=" text-black flex justify-center items-center h-screen w-screen bg-gray-50 gap-10">
       <div className="flex md:w-1/4 w-0">
@@ -13,26 +45,28 @@ export default function Login() {
       <div className="flex flex-col md:w-2/4 w-full max-w-md border border-gray-300 rounded py-20">
         <div className=" flex flex-col justify-center items-center">
                 <img className="w-[150px] h-[50px]" src={Logo} alt="" />
-                <form className="mt-8 w-64 flex flex-col">
-                  <input
+                <form onSubmit={handleSubmit(submitForm)} className="mt-8 w-64 flex flex-col">
+                  <input {...register("username")}
                     className="text-xs w-full mb-4 rounded border text-black bg-gray-100 border-gray-300 px-2 py-3 focus:outline-none focus:border-gray-400 active:outline-none"
-                    id="usesrname"
-                    placeholder="usesrname"
-                    type="usesrname"
+                    id="username"
+                    placeholder="username"
+                    type="text"
                   />
+                  { errors?.username?<span className="text-error"> {errors.username.message}</span>:null}
+
                   <input
+                  {...register("password")}
                     className="text-xs w-full mb-4 rounded border text-black bg-gray-100 border-gray-300 px-2 py-3 focus:outline-none focus:border-gray-400 active:outline-none"
                     id="password"
                     placeholder="Password"
                     type="password"
                   />
-                  <a className=" text-sm text-center bg-blue-300 text-white py-1 rounded font-medium">
-                    Login
-                  </a>
+                  { errors?.password?<span className="text-error"> {errors.password.message}</span>:null}
+                  <button  type="submit" className=" text-sm text-center bg-blue-300 text-white py-1 rounded font-medium">Login</button>
                 </form>
                 <div className=" text-black text-center w-80 pt-10">
                   <span className="text-sm">Don’t have an account? </span>
-                  <a className="text-blue-500 text-sm font-semibold">signup</a>
+                  <Link to="/signup" className="text-blue-500 text-sm font-semibold" >signup</Link>
                 </div>
             </div>
       </div>
